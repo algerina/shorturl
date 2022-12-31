37c189f117dd2306fa1f3c2bd35bc11af4aa65fd
Amel Khiri <53347966+algerina@users.noreply.github.com>
2022-12-31 18:12:49 +0100

    Change routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
-  get 'urls/index'
-  get 'urls/new'
-  get 'users/new'
-  get 'users/edit'
-  get 'users/show'
+  resources :urls, only: [:index, :new, :create, :show]
+  resources :users, only: [:index, :new, :create, :show]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
